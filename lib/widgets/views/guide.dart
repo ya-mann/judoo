576c138069e878dad0cf277273aac00e3d02ba43
ya-mann <76103946+ya-mann@users.noreply.github.com>
2020-12-17 17:20:50 +0100

    .

--- lib/widgets/views/guide.dart
+++ lib/widgets/views/guide.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 import '../navigationBar.dart';
@@ -7,9 +8,16 @@ class GuidePage extends StatelessWidget {
   static const name = 'Guide';
   static const icon = Icon(Icons.book, size: 26);
 
+  final databaseReference = FirebaseDatabase.instance.reference();
+
+  void getTechniques() async {
+    final techniques = await databaseReference.child("techniques").once();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
     );
   }
 }
--- lib/widgets/views/home.dart
+++ lib/widgets/views/home.dart
@@ -8,47 +8,53 @@ class NewsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-        color: Colors.white54,
-        borderRadius: BorderRadius.all(Radius.circular(15)),
-        child: InkWell(
-          onTap: () => launch(this.data.link, forceWebView: true),
-          borderRadius: BorderRadius.all(Radius.circular(15)),
-          //color: Colors.white54,
-          child: Padding(
-            padding: EdgeInsets.all(15),
-            child: Column(
-              children: [
-                RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                        text: this.data.title,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline4
-                            .copyWith(color: Colors.blue[900]))),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Padding(
+        padding: EdgeInsets.symmetric(vertical: 5),
+        child: Material(
+            color: Colors.white70,
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            child: InkWell(
+              onTap: () => launch(this.data.link, forceWebView: true),
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              //color: Colors.white54,
+              child: Padding(
+                padding: EdgeInsets.all(15),
+                child: Column(
                   children: [
-                    Text(this.data.tag, style: TextStyle(color: Colors.black54)),
-                    Text(this.data.date, style: TextStyle(color: Colors.black54))
+                    RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                            text: this.data.title,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline4
+                                .copyWith(color: Colors.blue[900]))),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(this.data.tag,
+                            style: TextStyle(color: Colors.black54)),
+                        Text(this.data.date,
+                            style: TextStyle(color: Colors.black54))
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: ClipRRect(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                                child: Image.network(this.data.thumbnailUrl))),
+                        Flexible(
+                            child: Text(this.data.description, softWrap: true))
+                      ],
+                    )
                   ],
                 ),
-                SizedBox(height: 15),
-                Row(
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.only(right: 10),
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            child: Image.network(this.data.thumbnailUrl))),
-                    Flexible(child: Text(this.data.description, softWrap: true))
-                  ],
-                )
-              ],
-            ),
-          ),
-        ));
+              ),
+            )));
   }
 }
 
@@ -59,38 +65,36 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final news = News.get(numberOfNews: 2);
+    final news = News.get(numberOfNews: 4);
     return Scaffold(
       body: FutureBuilder<List<News>>(
           future: news,
           builder: (BuildContext context, AsyncSnapshot<List<News>> snapshot) {
-            final newsWidgets = snapshot.hasData ? snapshot.data.map((data) {
-              return NewsWidget(data: data);
-            }).toList() : [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  color: Colors.white54,
-                ),
-                height: 150,
-                //borderRadius: BorderRadius.all(Radius.circular(15)),
-                child: Center(
-                  child: snapshot.hasError ? Icon(Icons.error_rounded, color: Colors.red) : CircularProgressIndicator(),
-                ),
-              )
-            ];
+            final newsWidgets = snapshot.hasData
+                ? snapshot.data.map((data) {
+                    return NewsWidget(data: data);
+                  }).toList()
+                : [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                        color: Colors.white54,
+                      ),
+                      height: 150,
+                      //borderRadius: BorderRadius.all(Radius.circular(15)),
+                      child: Center(
+                        child: snapshot.hasError
+                            ? Icon(Icons.error_rounded, color: Colors.red)
+                            : CircularProgressIndicator(),
+                      ),
+                    )
+                  ];
             return SafeArea(
               child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    ListView(
-                      padding: EdgeInsets.all(10),
-                      shrinkWrap: true,
-                      children: newsWidgets,
-                    )
-                  ],
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                child: ListView(
+                  shrinkWrap: true,
+                  children: newsWidgets,
                 ),
               ),
             );
