159998c54faa9a95cc712fbbc62719f0c69433dd
ya-mann <76103946+ya-mann@users.noreply.github.com>
2020-12-17 12:15:06 +0100

    .

--- lib/widgets/views/home.dart
+++ lib/widgets/views/home.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:judoo/helpers/getNews.dart';
-import 'package:judoo/widgets/navigationBar.dart';
+import 'package:judoo/helpers/news.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -15,97 +15,41 @@ class HomePage extends StatelessWidget {
           future: news,
           builder: (BuildContext context, AsyncSnapshot<News> snapshot) {
             final News data = snapshot.hasData ? snapshot.data : null;
-
             return SafeArea(
-              top: false,
-              bottom: false,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(6, 30, 6, 30),
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 380,
-                      child: Card(
-                        clipBehavior: Clip.antiAlias,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            // Photo and title.
-                            SizedBox(
-                              height: 245.0,
-                              child: Stack(
-                                children: <Widget>[
-                                  Positioned.fill(
-                                    child: Ink.image(
-                                      image: NetworkImage(data?.thumbnailUrl),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                  Positioned(
-                                    bottom: 16.0,
-                                    left: 16.0,
-                                    right: 16.0,
-                                    child: FittedBox(
-                                      fit: BoxFit.scaleDown,
-                                      alignment: Alignment.centerLeft,
-                                      child: Text(
-                                        data?.title,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .headline5
-                                            .copyWith(
-                                                color: Colors.white,
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.bold,
-                                                shadows: [
-                                              Shadow(
-                                                  color: Colors.black,
-                                                  blurRadius: 10,
-                                                  offset:
-                                                      Offset.fromDirection(0))
-                                            ]),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
+              child: Column(children: [
+                Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Text('Actualité',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w500))),
+                Card(
+                  child: Padding(
+                      padding: EdgeInsets.only(top: 20, bottom: 10),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          ListTile(
+                            leading: Image.network(data?.thumbnailUrl),
+                            title: RichText(
+                              text: TextSpan(
+                                  text: data?.title,
+                                  style: TextStyle(color: Colors.blue)),
                             ),
-                            // Description and share/explore buttons.
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  16.0, 16.0, 16.0, 0.0),
-                              child: DefaultTextStyle(
-                                softWrap: false,
-                                style: Theme.of(context).textTheme.subtitle1,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    // three line description
-                                    Padding(
-                                      padding:
-                                          const EdgeInsets.only(bottom: 8.0),
-                                      child: Text(
-                                        data?.description,
-                                        softWrap: true,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .subtitle1
-                                            .copyWith(color: Colors.black54),
-                                      ),
-                                    ),
-                                    Text(data?.tag, style: TextStyle(color: Colors.deepPurple[500])),
-                                    Text(data?.date, style: TextStyle(color: Colors.deepPurple[300]),),
-                                  ],
-                                ),
+                            subtitle: Text(data?.description),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              TextButton(
+                                child: const Text('En savoir plus...'),
+                                onPressed: () => debugPrint("pressed"),
                               ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+                            ],
+                          ),
+                        ],
+                      )),
+                )
+              ]),
             );
           }),
     );
