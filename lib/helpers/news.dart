ec447f33f492a022f84275a1a55b3ecc69a124dd
ya-mann <76103946+ya-mann@users.noreply.github.com>
2020-12-17 16:01:11 +0100

    .

--- lib/helpers/news.dart
+++ lib/helpers/news.dart
@@ -4,23 +4,32 @@ import 'package:http/http.dart' as http;
 
 class News {
   String link, title, thumbnailUrl, description, tag, date;
-  News({this.link, this.title, this.thumbnailUrl, this.description, this.tag, this.date});
+  News(
+      {this.link,
+      this.title,
+      this.thumbnailUrl,
+      this.description,
+      this.tag,
+      this.date});
 
-  static Future<News> get() async {
+  static Future<List<News>> get({numberOfNews: 1}) async {
     final response =
         await http.get(Uri.parse('https://www.ffjudo.com/actualites/'));
-    News news;
+    List<News> news = [];
     if (response.statusCode == 200) {
       final document = parse(utf8.decode(response.bodyBytes));
-      final newsHeader = document.querySelector('.listing_bloc a');
-      news = News(
-        link: newsHeader.attributes['href'],
-        title: newsHeader.text,
-        thumbnailUrl: document.querySelector('.listing_bloc img').attributes['src'],
-        description: document.querySelector('.listing_bloc p.clearfix').text,
-        tag: document.querySelector('.listing_bloc .lieux').text,
-        date:  document.querySelector('.listing_bloc .date').text.substring(3),
-      );
+      final newsBlock = document.querySelectorAll('.listing_bloc').getRange(0, numberOfNews);
+      newsBlock.forEach((element) {
+        final newsHeader = element.getElementsByTagName('a').first;
+        news.add(News(
+          link: newsHeader.attributes['href'],
+          title: newsHeader.text,
+          thumbnailUrl: element.getElementsByTagName('img').first.attributes['src'],
+          description: element.querySelector('p.clearfix').text,
+          tag: element.getElementsByClassName('lieux').first.text,
+          date: element.getElementsByClassName('date').first.text.substring(3),
+        ));
+      });
     }
     return news;
   }
--- lib/widgets/views/home.dart
+++ lib/widgets/views/home.dart
@@ -2,6 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:judoo/helpers/news.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+class NewsWidget extends StatelessWidget {
+  News data;
+  NewsWidget({this.data, Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.white54,
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+        child: InkWell(
+          onTap: () => launch(this.data.link, forceWebView: true),
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+          //color: Colors.white54,
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: [
+                RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                        text: this.data.title,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline4
+                            .copyWith(color: Colors.blue[900]))),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(this.data.tag, style: TextStyle(color: Colors.black54)),
+                    Text(this.data.date, style: TextStyle(color: Colors.black54))
+                  ],
+                ),
+                SizedBox(height: 15),
+                Row(
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.only(right: 10),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            child: Image.network(this.data.thumbnailUrl))),
+                    Flexible(child: Text(this.data.description, softWrap: true))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
   static const name = 'Accueil';
@@ -9,12 +59,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final news = News.get();
+    final news = News.get(numberOfNews: 2);
     return Scaffold(
-      body: FutureBuilder<News>(
+      body: FutureBuilder<List<News>>(
           future: news,
-          builder: (BuildContext context, AsyncSnapshot<News> snapshot) {
-            final News data = snapshot.hasData ? snapshot.data : null;
+          builder: (BuildContext context, AsyncSnapshot<List<News>> snapshot) {
+            final newsWidgets = snapshot.hasData ? snapshot.data.map((data) {
+              return NewsWidget(data: data);
+            }).toList() : [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  color: Colors.white54,
+                ),
+                height: 150,
+                //borderRadius: BorderRadius.all(Radius.circular(15)),
+                child: Center(
+                  child: snapshot.hasError ? Icon(Icons.error_rounded, color: Colors.red) : CircularProgressIndicator(),
+                ),
+              )
+            ];
             return SafeArea(
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
@@ -22,61 +86,9 @@ class HomePage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
                     ListView(
+                      padding: EdgeInsets.all(10),
                       shrinkWrap: true,
-                      children: [
-                        Material(
-                            color: Colors.white54,
-                            borderRadius: BorderRadius.all(Radius.circular(15)),
-                            child: InkWell(
-                              onTap: () => launch(data?.link, forceWebView: true),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                              //color: Colors.white54,
-                              child: Padding(
-                                padding: EdgeInsets.all(15),
-                                child: Column(
-                                  children: [
-                                    RichText(
-                                        textAlign: TextAlign.center,
-                                        text: TextSpan(
-                                            text: data?.title,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .headline4
-                                                .copyWith(
-                                                    color: Colors.blue[900]))),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(data?.tag,
-                                            style: TextStyle(
-                                                color: Colors.black54)),
-                                        Text(data?.date,
-                                            style: TextStyle(
-                                                color: Colors.black54))
-                                      ],
-                                    ),
-                                    SizedBox(height: 15),
-                                    Row(
-                                      children: [
-                                        Padding(
-                                            padding: EdgeInsets.only(right: 10),
-                                            child: ClipRRect(
-                                                borderRadius: BorderRadius.all(
-                                                    Radius.circular(10)),
-                                                child: Image.network(
-                                                    data?.thumbnailUrl))),
-                                        Flexible(
-                                            child: Text(data?.description,
-                                                softWrap: true))
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              ),
-                            )),
-                      ],
+                      children: newsWidgets,
                     )
                   ],
                 ),
